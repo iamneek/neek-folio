3591ed59f337dd227e341035d265006df096282b
Neek Kafle <136208577+iamneek@users.noreply.github.com>
2026-08-19 23:22:17 +0545

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "~/neek",
-  EMAIL: "neekkafle29@gmail.com",
+  EMAIL: "neek@neek.com.np",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
